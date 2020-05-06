6b6d7b4db49cef4a744e1330372a39bb65aaafbe
Kevin Lahoda <klahoda@binghamton.edu>
2020-05-06 17:18:24 -0400

    bug fix

--- data-portrait/js/survey.js
+++ data-portrait/js/survey.js
@@ -17,34 +17,37 @@ class Answer {
 
   // Display the Response
   display(r) {
+
     if(r == this.q) {
       if(!this.is_text) {
-          //console.log('not ok:'+ this.img);
+        //console.log('not ok:'+ this.imgtext);
           image(this.img, this.x, this.y);
-          //console.log('s');
       }
     }
   }
 
   displayText(r) {
+
     if(r == this.q) {
       if(this.is_text) {
         //console.log('ok');
         fill(239, 173, 27);
         textSize(43);
-        textFont('C-Medium');
+        textFont('Helvetica');
         textLeading(60)
         text(this.imgtext+'!', this.x, this.y, 460);
         //
       }
     }
+
+    
+
   }
 
 }
 
 let data = {}; // Global object to hold results from the loadJSON call
 let answers = []; // Global array to hold all answer objects
-var bg;
 
 // Put any asynchronous data loading in preload to complete before "setup" is run
 function preload() {
@@ -60,6 +63,8 @@ function loadData(r) {
   let responseData = data['data'];
   let responseKey = data['key'];
 
+  //console.log(responseKey);
+
   for (let i = 0; i < responseData.length; i++) {
     //console.log(i+'-------------------------------');
     $("#nav").append('<li><a id="'+i+'" href="#">'+i+'</a></li>');
@@ -69,22 +74,36 @@ function loadData(r) {
     var res = Object.entries(response);
 
     for (const [question, answer] of res) {
-      //console.log(`${question} and ${answer}`);
+      console.log(`${question} and ${answer}`);
       let imgtext = '';
       let key = responseKey[question];
+      let ans = key[answer];
       let x = key['x'];
       let y = key['y'];
       let is_text = key['is_text'];
+      let determined_by = key['determined_by'];
+
+      if(determined_by){
+        console.log('det? '+determined_by);
+        let det = responseKey[determined_by]
+        let q = response[determined_by]
+        let imdet = det[q]
+        imdet = imdet.substr(0, imdet.lastIndexOf("."))
+        let imkey = key[answer];
+        imkey = imkey.substr(0, imkey.lastIndexOf("."))
+        console.log(imkey+imdet+'.png');
+        ans = imkey+imdet+'.png';
+      }
 
       if(is_text == true){
         //console.log(answer);
         imgtext = answer;
       } else {
-        imgtext = key[answer];
+        imgtext = ans;
         //console.log(imgtext);
       }
 
-      //console.log(i+' - '+x+' - '+y+' - '+imgtext+' - '+ is_text)
+      //console.log(x)
       // Put object in array
       answers.push(new Answer(i, x, y, imgtext, is_text));
     }
@@ -102,18 +121,18 @@ function loadData(r) {
   
 }
 
+
 function setup() {
   var canvas = createCanvas(500, 500);
   canvas.parent('sketch');
 }
 
 function draw() {
-  //background(255,255,255);
+  background(255);
   image(bg, 0, 0);
 
   // Display all answers
   for (let i = 0; i < answers.length; i++) {
-    console.log(i);
     answers[i].display(responseIndex);
     answers[i].displayText(responseIndex);
   }
